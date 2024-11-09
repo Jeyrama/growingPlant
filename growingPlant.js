7756b1685610f978677f74697c93efe399c3611c
Jey <126204426+Jeyrama@users.noreply.github.com>
2024-11-08 20:40:27 -0800

    Update growingPlant.js

--- growingPlant.js
+++ growingPlant.js
@@ -28,3 +28,19 @@ Input/Output:
 
 
 // Solution
+
+function growingPlant(upSpeed, downSpeed, desiredHeight) {
+  if (downSpeed > upSpeed) {
+    return -1;
+  }
+  
+  let height = downSpeed;
+  let days = 0;
+  do {
+    days++;
+    height -= downSpeed;
+    height += upSpeed;
+  } while (height < desiredHeight)
+  
+  return days;
+}
